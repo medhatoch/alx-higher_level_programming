0263dd3b85adb80cc1512d0408af371764c2b3fc
root <medamine.hatoch@gmail.com>
2023-08-11 15:03:31 -0700

    question 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the result of the addition 1 + 2."""
+    from add_0 import add
 
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")

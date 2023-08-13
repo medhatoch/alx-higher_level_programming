9273033a7c99093a413636405a1454311ad74322
root <medamine.hatoch@gmail.com>
2023-08-13 05:15:27 -0700

    question 1 test 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    "Print the sum, diff, mult and quot of 10 and 5."
+    from calculator_1 import add, sub, mul, div
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
+    a = 10
+    b = 5
 
-print(f"{a} + {b} = {add_result}")
-print(f"{a} - {b} = {sub_result}")
-print(f"{a} * {b} = {mul_result}")
-print(f"{a} / {b} = {div_result}")
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+
+    print("{} + {} = {}".format(a, b, add_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))

319c594f82def3c4f4f149e9134f2079e2ffe10f
root <medamine.hatoch@gmail.com>
2023-08-30 09:54:02 -0700

    q0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self):
+        """Initialize an empty square."""
+        pass
